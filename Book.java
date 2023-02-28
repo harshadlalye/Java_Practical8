c97afe3cd7e9c8b6c91525f7726e73dabf530867
Harshad Lalye <harshadlalye@gmail.com>
2023-02-28 15:20:02 +0530

    Added User Input

--- Book.java
+++ Book.java
@@ -1,6 +1,8 @@
 /* Write a Java program to create class 'Book'. 
 User can add infromation of Book using function.
 Bookid, name, author, publisher */
+import java.util.Scanner;
+
 public class Book {
     private int bookId;
     private String name;
@@ -14,41 +16,59 @@ public class Book {
         this.publisher = publisher;
     }
     
-    public void setBookId(int bookId) {
-        this.bookId = bookId;
-    }
-    
     public int getBookId() {
         return bookId;
     }
     
-    public void setName(String name) {
-        this.name = name;
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
     }
     
     public String getName() {
         return name;
     }
     
-    public void setAuthor(String author) {
-        this.author = author;
+    public void setName(String name) {
+        this.name = name;
     }
     
     public String getAuthor() {
         return author;
     }
     
-    public void setPublisher(String publisher) {
-        this.publisher = publisher;
+    public void setAuthor(String author) {
+        this.author = author;
     }
     
     public String getPublisher() {
         return publisher;
     }
     
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+    
     public static void main(String[] args) {
-        // Example usage:
-        Book myBook = new Book(123, "The Great Gatsby", "F. Scott Fitzgerald", "Scribner");
-        System.out.println(myBook.getName() + " by " + myBook.getAuthor() + " (published by " + myBook.getPublisher() + ")");
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter book ID: ");
+        int bookId = input.nextInt();
+        input.nextLine();
+        
+        System.out.print("Enter book name: ");
+        String name = input.nextLine();
+        
+        System.out.print("Enter book author: ");
+        String author = input.nextLine();
+        
+        System.out.print("Enter book publisher: ");
+        String publisher = input.nextLine();
+        
+        Book book = new Book(bookId, name, author, publisher);
+        System.out.println("Book added successfully!");
+        System.out.println("Book ID: " + book.getBookId());
+        System.out.println("Book name: " + book.getName());
+        System.out.println("Book author: " + book.getAuthor());
+        System.out.println("Book publisher: " + book.getPublisher());
     }
 }
